16aaa6c98e6b521890a7a2e59f59f7b948d0c240
Matiello <matiello@users.noreply.github.com>
2018-07-02 21:55:10 -0300

    Update AppTest.java

--- qa-diag-core/src/test/java/com/insightx/tools/diagnostic/AppTest.java
+++ qa-diag-core/src/test/java/com/insightx/tools/diagnostic/AppTest.java
@@ -1,12 +1,25 @@
 package com.insightx.tools.diagnostic;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import junit.framework.TestCase;
 
-public class AppTest {
+public class AppTest extends TestCase {
+  private java.util.List emptyList;
 
-  @Test
-  public void evaluatesExpression() {
+  /**
+   * Sets up the test fixture. 
+   * (Called before every test case method.)
+   */
+  public void setUp() {
+  }
+
+  /**
+   * Tears down the test fixture. 
+   * (Called after every test case method.)
+   */
+  public void tearDown() {
+  }
+  
+  public void testSomeBehavior() {
     AppCLI.main("AppCLI", "-h");
     assertEquals(1, 1);
   }
